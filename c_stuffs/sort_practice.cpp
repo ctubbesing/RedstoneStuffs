4290db25a96bcd851a85e17ac55a5ae1865235a1
Connor Tubbesing <connortubbesing@gmail.com>
2021-09-25 16:39:40 -0500

    clean up c++ stuffs

--- c_stuffs/sort_practice.cpp
+++ c_stuffs/sort_practice.cpp
@@ -98,6 +98,78 @@ void quickSort(vector<int> &list)
     cout << qCount << " swaps." << endl;
 }
 
+int b_qSort_split(vector<int> &list, int start, int end)
+{
+    int pivot = list[end];
+    int left = start;
+    int right = end - 1;
+
+    while (left <= right) {
+        // find a pair to swap
+        while (list[left] < pivot && left < end) {
+            left++;
+        }
+        while (list[right] > pivot && right >= start) {
+            right--;
+        }
+
+        // swap pair or put pivot into position
+        if (left < right) {
+            int swapVal = list[left];
+            list[left] = list[right];
+            list[right] = swapVal;
+        }
+        else {
+            list[end] = list[left];
+            list[left] = pivot;
+        }
+    }
+
+    return left;
+}
+
+void b_qSort_sublist(vector<int> &list, int start, int end)
+{
+    // base case
+    if (start >= end) {
+        return;
+    }
+
+    // split into sublists
+    int pivotPos = b_qSort_split(list, start, end);
+
+    // recursively sort sublists
+    b_qSort_sublist(list, start, pivotPos - 1);
+    b_qSort_sublist(list, pivotPos + 1, end);
+}
+
+void b_quickSort(vector<int> &list)
+{
+    b_qSort_sublist(list, 0, list.size() - 1);
+}
+
+void b_bubbleSort(vector<int> &list)
+{
+    int n = list.size();
+    int swaps = 0;
+    int loops = 0;
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n - (i + 1); j++) {
+            // swap if necessary
+            if (list[j] > list[j + 1]) {
+                int swapVal = list[j];
+                list[j] = list[j + 1];
+                list[j + 1] = swapVal;
+
+                swaps++;
+            }
+            loops++;
+        }
+    }
+    cout << swaps << " swaps, " << loops << " loops" << endl;
+}
+
 int main()
 {
     // get list to sort
@@ -110,8 +182,8 @@ int main()
 
     // sort list
     vector<int> sortedList = list;
-    // bubbleSort(sortedList);
-    quickSort(sortedList);
+    b_bubbleSort(sortedList);
+    // quickSort(sortedList);
 
     // return sorted list
     cout << "Sorted list:   ";
